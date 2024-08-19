6fa7a20986ebe800698b8d7ff5575f8638e10eeb
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-19 18:25:54 +0200

    Button

---
+++ CustomButtonStyle/CustomButtonStyle/ButtonStyle.swift
@@ -0,0 +1,41 @@
+//
+//  ButtonStyle.swift
+//  CustomButtonStyle
+//
+//  Created by Manyuchi, Carrington C on 2024/08/19.
+//
+
+import UIKit
+
+
+
+
+// MARK: - Button Style
+
+
+class CMButtonStyle: UIButton {
+    
+    @IBInspectable var setTitle: String = "" {
+        didSet {
+            self.setTitle(setTitle, for: .normal)
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.borderWidth = 1
+        }
+    }
+    
+    @IBInspectable var buttonCornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = buttonCornerRadius
+        }
+    }
+    
+    @IBInspectable var backGroundColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.backgroundColor = backGroundColor.cgColor
+        }
+    }
+}
---
+++ CustomButtonStyle/CustomButtonStyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomButtonStyle
+//
+//  Created by Manyuchi, Carrington C on 2024/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
